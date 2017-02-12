c8ad583d294516f5eb75d0f374feb2d981295771
Andrés Duarte M <andres.duarte.marquez@gmail.com>
2017-02-12 01:28:48 -0300

    $rootScope y Child Controllers

---
+++ child/controller.js
@@ -0,0 +1,10 @@
+angular.module("MyFirstApp", [])
+	.run(function($rootScope){
+		$rootScope.nombre = "Codigo Facilito";
+	})
+	.controller("FirstController", function($scope){
+		$scope.nombre = "CF";
+	})
+	.controller("ChildController", function($scope){
+		
+	})
